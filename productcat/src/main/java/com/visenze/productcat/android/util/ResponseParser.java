4b9acfac20af67ddecb42a2f245a8aeebb583664
Ngo Hung <hung@visenze.com>
2018-09-11 00:42:05 +0800

    add visual_score in response

--- productcat/src/main/java/com/visenze/productcat/android/util/ResponseParser.java
+++ productcat/src/main/java/com/visenze/productcat/android/util/ResponseParser.java
@@ -235,6 +235,7 @@ public class ResponseParser {
 
                 productSummary.setPriceString(summary.optString("price"));
                 productSummary.setSellerName(summary.optString("seller_name"));
+                productSummary.setVisualScore(summary.optDouble("visual_score"));
 
                 if (summary.has("stores")) {
                     JSONArray storesJson = summary.getJSONArray("stores");
